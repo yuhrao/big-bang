8a3e19d7bc715b9a9c5cb3ffdc879a39b4da507d
Yuhri Graziano Bernardes <yuhri.graziano@gmail.com>
2023-07-16 20:53:38 -0300

    [CMP-logger] feat: create component
    [CMP-logger] test: fix test runner
    
    reformat deps.edn
    
    Signed-off-by: Yuhri Graziano Bernardes <yuhri.graziano@gmail.com>

---
+++ components/logger/src/br/dev/yuhri/logger/logger.clj
@@ -0,0 +1,70 @@
+(ns br.dev.yuhri.logger.logger
+  (:require [br.dev.yuhri.logger.publishers :as pubs]
+            [com.brunobonacci.mulog :as u]))
+
+(defonce current-logger (atom {:stop   nil
+                               :status :down
+                               :opts   nil}))
+
+(def ^:private publisher-mappings
+  {:console pubs/console-publisher
+   :file    pubs/file-publisher})
+
+(def ^:private log-levels
+  {:debug 0
+   :info  1
+   :warn  2
+   :error 3})
+
+(defn- keep-log-by-level? [min-level {:keys [level]}]
+  (>= (log-levels level) (log-levels min-level)))
+
+(defn filter-log-level-xfn [{:keys [min-level]
+                             :or   {min-level :info}}]
+  (filter (partial keep-log-by-level? min-level)))
+
+(defn- default-transform-xfns [opts]
+  [(filter-log-level-xfn opts)])
+
+(defn- create-transform-fn [xfns]
+  (let [xfn (apply comp xfns)]
+    (fn [events]
+      (sequence xfn events))))
+
+(defn- get-standard-publishers [pub pub-cfg]
+  (if-let [pub-fn (publisher-mappings pub)]
+    (pub-fn pub-cfg)
+    pub-cfg))
+
+(defn- create-publishers [{:keys [publishers] :as opts}]
+  (let [default-xfns (default-transform-xfns opts)]
+    (->> publishers
+         (keep (fn [[pub pub-cfg]]
+                 (let [{:keys [xfns] :as publisher} (get-standard-publishers pub pub-cfg)
+                       transform-fn (create-transform-fn (concat default-xfns xfns))]
+                   (-> publisher
+                       (assoc :transform transform-fn))))))))
+
+(defn setup! [opts]
+  (when (= :down (-> current-logger
+                     deref
+                     :status))
+    (let [publishers (create-publishers opts)
+          publishers {:type       :multi
+                      :publishers publishers}]
+      (reset! current-logger
+              {:stop   (u/start-publisher! publishers)
+               :status :up
+               :opts   opts}))))
+
+(defn stop! []
+  (let [{:keys [stop status] :as old} @current-logger]
+    (when (= status :up)
+      (stop)
+      (reset! current-logger (assoc old :status :down)))))
+
+(defn restart! []
+  (let [{:keys [status opts]} @current-logger]
+    (when (= status :up)
+      (stop!)
+      (setup! opts))))
