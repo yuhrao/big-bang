43f6f59b69dc2cd9e3a45e6c340292327378ad20
Yuhri Graziano Bernardes <yuhri.graziano@gmail.com>
2023-12-29 23:17:35 -0300

    refact(c/http-client): migrate to babashka http client

--- components/http-client/src/br/dev/yuhri/http_client/request.clj
+++ components/http-client/src/br/dev/yuhri/http_client/request.clj
@@ -1,7 +1,5 @@
 (ns br.dev.yuhri.http-client.request
-  (:require [camel-snake-kebab.core :as csk]
-            [camel-snake-kebab.extras :as cske]
-            [clj-http.client :as http.client]
+  (:require [babashka.http-client :as bb.http]
             [clojure.string :as string])
   (:import (clojure.lang ExceptionInfo)))
 
@@ -10,47 +8,24 @@
     path
     (str "/" path)))
 
-(defn- encode-request [{{encode :encode-fn} :content-negotiation} req]
-  (encode req))
 
-(defn- normalize-req-header [{:keys [headers] :as req}]
-  (let [{:keys [content-type] :as headers}
-        (cond-> req
-                headers
-                (update :headers
-                        (partial cske/transform-keys csk/->HTTP-Header-Case-String)))]
-    (if content-type
-      headers
-      (assoc headers :content-type "application/json"))))
-
-(defn- normalize-res-header [{:keys [headers] :as res}]
-  (cond-> res
-          headers (update :headers
-                          (partial cske/transform-keys csk/->kebab-case-keyword))))
-
-(defn- prepare-request [client {:keys [path headers]
+(defn- prepare-request [client {:keys [path]
                                 :as   req}]
   (let [base-url (->> [client req]
-                      (map :base-url)
-                      (remove nil?)
+                      (keep :base-url)
                       first)
-        url      (format "%s%s" base-url (prepare-path path))
-        body     (encode-request client req)]
-    (cond-> req
-            true (dissoc :path)
-            true (assoc :url url)
-            true (normalize-req-header)
-            body (assoc :body body))))
-
-(defn- handle-response [{{decode :decode-fn} :content-negotiation} res]
-  (let [body (decode res)]
-    (cond-> res
-            body (assoc :body body)
-            true normalize-res-header)))
+        url      (str base-url (prepare-path path))]
+    (merge
+      (:req client)
+      (-> req
+          (dissoc :path)
+          (assoc :interceptors (:interceptors client))
+          (assoc :throw false)
+          (assoc :uri url)))))
 
 (defn- safe-request [req]
   (try
-    (assoc (http.client/request req) :success? true)
+    (assoc (bb.http/request req) :success? true)
     (catch ExceptionInfo e
       (assoc (ex-data e)
         :success?
@@ -60,5 +35,4 @@
 
 (defn execute [client req]
   (->> (prepare-request client req)
-       safe-request
-       (handle-response client)))
+       safe-request))
