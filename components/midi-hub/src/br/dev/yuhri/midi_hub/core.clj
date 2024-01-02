7378ff2b211b7620f6a0073e420b30e94e86fc28
Yuhri Graziano Bernardes <yuhri.graziano@gmail.com>
2024-01-02 11:58:57 -0300

    feat (c/midi-hub): create interface for main operations

--- components/midi-hub/src/br/dev/yuhri/midi_hub/core.clj
+++ components/midi-hub/src/br/dev/yuhri/midi_hub/core.clj
@@ -1,5 +1,18 @@
-(ns br.dev.yuhri.midi-hub.core)
+(ns br.dev.yuhri.midi-hub.core
+  (:require [br.dev.yuhri.midi-hub.lib.db :as mh.db]
+            [br.dev.yuhri.midi-hub.lib.openmidi :as openmidi]))
 
+(defn list-brands
+  "You can pass an option map with Honey SQL DSL"
+  ([] (mh.db/list-brands))
+  ([hsql-dql]
+   (mh.db/list-brands hsql-dql)))
 
-(defn list-brands []
-  )
+(defn list-devices
+  "You can pass an option map with Honey SQL DSL"
+  ([] (mh.db/list-devices))
+  ([hsql-dql]
+   (mh.db/list-devices hsql-dql)))
+
+(defn sync-openmidi []
+  (openmidi/import-specs))
