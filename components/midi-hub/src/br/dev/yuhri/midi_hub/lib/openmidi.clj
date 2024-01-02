aca69105e62beb99883b9e9c43f09e7a5b00eaad
Yuhri Graziano Bernardes <yuhri.graziano@gmail.com>
2024-01-02 11:47:30 -0300

    feat (c/midi-hub): import data from openmidi
    make some fns public

--- components/midi-hub/src/br/dev/yuhri/midi_hub/lib/openmidi.clj
+++ components/midi-hub/src/br/dev/yuhri/midi_hub/lib/openmidi.clj
@@ -3,14 +3,16 @@
             [camel-snake-kebab.core :as csk]
             [camel-snake-kebab.extras :as cske]
             [clojure.string :as string]
-            [medley.core :as medley]))
+            [br.dev.yuhri.midi-hub.lib.db :as mh.db]
+            [medley.core :as medley]
+            [clojure.set :as set]))
 
 (defn- get-brands []
   (->> "https://raw.githubusercontent.com/Morningstar-Engineering/openmidi/master/data/mapping.json"
        slurp
        json/json->clj
        :brands
-       (map #(update % :value csk/->kebab-case-keyword))))
+       (map #(set/rename-keys % {:name :brand-name}))))
 
 (defn- extract-kv [[k v]]
   (let [letters-re #"([a-zA-Z]+)"]
@@ -20,31 +22,36 @@
       [k v])))
 
 (defn- assoc-value-mappings [{:keys [description] :as param}]
-  (cond
-    (string/blank? description)
-    param
+  (try
+    (cond
+      (not (string? description)) param
 
-    (re-find #"(?i)value.?\d.?-.?+|(\w+).?=.\w+" description)
-    (->> (string/split description #"\n|,\n?")
-         (map #(string/split % #".?-.?|.?=.?"))
-         (map (fn [kv]
-                (let [[k v] (extract-kv kv)]
-                  [(-> k
-                       (string/replace #"(?i)value." "")
-                       string/trim
-                       string/trim-newline)
+      (string/blank? description)
+      param
 
-                   (-> v string/trim string/trim-newline)])))
-         (into {})
-         (assoc param :value-mapping))
+      (re-find #"(?i)(value)?.?\d\s?-\s?+|(\w+)\s?=\s?\w+" description)
+      (->> (string/split description #"\n|,\n?|\s?,\s?")
+           (map #(string/split % #"-|="))
+           (map (fn [kv]
+                  (let [[k v] (extract-kv kv)]
+                    [(-> k
+                         (string/replace #"(?i)value." "")
+                         string/trim
+                         string/trim-newline)
 
-    :else
-    param))
+                     (-> v string/trim string/trim-newline)])))
+           (into {})
+           (assoc param :value-mapping))
+
+      :else
+      param)
+    (catch Throwable e
+      param)))
 
 (defn deep-remove-empty [v]
   (let [empty-fn #(cond
-                    (coll? %) empty?
                     (string? %) string/blank?
+                    (coll? %) empty?
                     :else nil?)]
     (cond
       (map? v) (->> v
@@ -57,17 +64,40 @@
       (coll? v) (->> v
                      (map deep-remove-empty)
                      (remove #((empty-fn %) %)))
-      :else (when (not ((empty-fn v) v))
+      (string? v) (when (not ((empty-fn v) v))
+                    v)
+      :else (when (not (nil? v))
               v))))
 
-(defn- deep-trim-strings [v]
+(defn deep-trim-strings [v]
   (cond
     (string? v) (->> v
                      string/trim
                      string/trim-newline)
     (map? v) (medley/map-vals deep-trim-strings v)
-    (coll? v) (map deep-trim-strings v)))
+    (coll? v) (map deep-trim-strings v)
+    :else v))
 
+(defn- device-missing-data [{:keys [device-name value midi-specs io-specs] :as device}]
+  (cond-> #{}
+
+          (string/blank? device-name)
+          (conj :device-name)
+
+          (empty? midi-specs)
+          (conj :midi-specs)
+
+          (empty? io-specs)
+          (conj :io-specs)))
+
+(defn- make-device [{:keys [value] :as device}]
+  (let [missing-data (device-missing-data device)]
+   (cond-> device
+
+           (seq missing-data)
+           (assoc-in [:metadata :missing-data] (sort (vec missing-data)))
+
+           (missing-data :device-name) (assoc :device-name value))))
 
 (defn- get-all-devices-specs []
   (->> "https://raw.githubusercontent.com/Morningstar-Engineering/openmidi/master/data/all.json"
@@ -75,20 +105,36 @@
        (#(json/json->clj % {:key-fn identity}))
        (mapcat (fn [[brand devices]]
                  (map #(assoc (second %)
-                         :value (csk/->snake_case_string (first %))
-                         :brand brand) devices)))
+                         :value (first %)
+                         :brand-value brand) devices)))
        (map (partial cske/transform-keys csk/->kebab-case-keyword))
        (map deep-trim-strings)
        (map (fn [{:keys [cc] :as device}]
               (assoc device :cc (map assoc-value-mappings cc))))
        (map #(-> %
-                 (select-keys [:brand :device-name :value])
+                 (select-keys [:brand-value :device-name :value])
                  (assoc :io-specs (select-keys % [:midi-in :midi-thru :phantom-power])
                         :midi-specs (select-keys % [:midi-clock :pc :cc :midi-channel]))))
-       (map deep-remove-empty)))
+       (map deep-remove-empty)
+       (map make-device)))
 
 (defn- get-specs []
   (let [brands        (get-brands)
         devices-specs (get-all-devices-specs)]
     {:brands  brands
      :devices devices-specs}))
+
+(defn import-specs []
+  (let [{:keys [devices brands]} (get-specs)
+        brands          (for [brand brands]
+                          (merge brand (mh.db/upsert-brand (dissoc brand :models))))
+        brands-value-id (->> brands
+                             (map (juxt :value :id))
+                             (into {}))]
+    (doseq [device devices]
+      (tap> (mh.db/upsert-device (-> device
+                                (assoc
+                                  :brand-id
+                                  (brands-value-id (:brand-value device)))
+                                (dissoc :brand-value)))))
+    nil))
