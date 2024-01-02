1ad4d6db2e2d45ea62809d22f354d11fe7184c11
Yuhri Graziano Bernardes <yuhri.graziano@gmail.com>
2024-01-02 11:48:31 -0300

    dev: add one-time run script for database migrations

--- development/src/dev/database.clj
+++ development/src/dev/database.clj
@@ -12,30 +12,45 @@
               :out *out*
               :err :out}
              "clojure" "-X:migration" "make" ":jdbc-url" jdbc-url)
-    (println "Component not found:" (name component))))
+    (println "Component not found:" (name component)))
+  nil)
 
 (defn make-migrations []
   (for [component (keys configs)]
     (do
       (println "Making migration:" (name component))
-      (make-migration component)
-      nil)))
+      (make-migration component)))
+  nil)
 
 (defn run-migration [component]
   (if-let [jdbc-url (configs component)]
-    (p/shell {:dir (str "components/" (name component))
-              :out *out*
-              :err :out}
-             "clojure" "-X:migration" "migrate" ":jdbc-url" jdbc-url)
-    (println "Component not found:" (name component))))
+    (do
+      (p/shell {:dir (str "components/" (name component))
+                :out *out*
+                :err :out}
+               "clojure" "-X:migration" "migrate" ":jdbc-url" jdbc-url))
+    (println "Component not found:" (name component)))
+  nil)
 
 (defn run-migrations []
   (for [component (keys configs)]
-    (run-migration component)))
+    (run-migration component))
+  nil)
+
+
+(defn do-migration [component]
+  (make-migration component)
+  (run-migration component))
+
+(defn do-migrations []
+  (for [component (keys configs)]
+    (do-migration component)))
 
 (comment
   (make-migrations)
 
   (run-migrations)
+
+  (do-migrations)
 
   )
