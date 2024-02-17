868522fc99afb6f020df46a65f988d305ad4c76a
Yuhri Graziano Bernardes <yuhri.graziano@gmail.com>
2024-02-16 21:30:20 -0300

    doc(data-cloak): update docs

--- components/data-cloak/src/yuhrao/data_cloak/core/string.clj
+++ components/data-cloak/src/yuhrao/data_cloak/core/string.clj
@@ -2,10 +2,16 @@
   (:require [yuhrao.data-cloak.string :as dc.impl.string]))
 
 (defn offset
-  "Given a string, obscure all the string content from `start`+`start-offset` to `end`-`end-offset>
+  "Given a string, obscure all the string content from `start`+`start-offset` to `end`-`end-offset`>
   opts:
   - `:start`: amount of chars at the beginning of the string to not be obscured
-  - `:end`: amount of chars at the end of the string to not be obscured"
+  - `:end`: amount of chars at the end of the string to not be obscured
+
+  ```clojure
+  (dc.string/offset \"1234567890\" {:start 3 :end 4}) ;;=> \"123***4890\")
+  ```
+
+  "
   [s opts]
   (dc.impl.string/offset s opts))
 
@@ -15,7 +21,12 @@
   (dc.impl.string/symmetric-offset s n))
 
 (defn all
-  "Obscure the whole string. Useful for passwords"
+  "Obscure the whole string. Useful for passwords
+
+  Example:
+  ```clojure
+  (dc.string/all \"1234567890\") ;;=> \"**********\"
+  ```"
   [s]
   (dc.impl.string/all s))
 
@@ -29,6 +40,11 @@
 
 (defn phone
   "Obscure a phone number. It removes phone number's specific special characters
-  such as `()+` and white spaces"
+  such as `()+` and white spaces
+
+  Examples:
+  - +55 (11) 91234-1234 -> 55119****1234
+  - (11) 91234-1234 -> 119****1234
+  - 91234-1234 -> *****1234"
   [s]
   (dc.impl.string/phone s))
